d7002cb250e993598fe263d2ffbba2d1a2c9c4dc
gustarhs <gustasecundaria2011@gmail.com>
2026-07-06 13:49:21 -0300

    Criação model triagem

---
+++ backend/models/triagem.py
@@ -0,0 +1,19 @@
+from database import db
+
+class TriagemModel(db.Model):
+    __tablename__ = 'triagem'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    data_hora = db.Column(db.DateTime, nullable=False)
+    resultado = db.Column(db.Boolean, nullable=False)
+    perguntas = db.Column(db.Text, nullable=False)  # LONGTEXT mapeia para Text no SQLAlchemy
+    respostas = db.Column(db.Text, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "data_hora": self.data_hora.isoformat(),
+            "resultado": self.resultado,
+            "perguntas": self.perguntas,
+            "respostas": self.respostas
+        }
